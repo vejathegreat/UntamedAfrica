fe6b136a143045e804d3cd9ea9ac61a74b619d40
veli <velly_mathebula@rocketmail.com>
2019-08-04 10:55:44 +0200

    added challenges

--- app/src/main/java/com/velaphi/untamed/features/animalDetails/OtherFragment.java
+++ app/src/main/java/com/velaphi/untamed/features/animalDetails/OtherFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.velaphi.untamed.R;
+import com.velaphi.untamed.features.animalDetails.adapters.ChallangesAdapter;
 import com.velaphi.untamed.features.animalDetails.models.AnimalDetailsModel;
 
 public class OtherFragment extends Fragment {
@@ -22,7 +25,17 @@ public class OtherFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_other, container, false);
         animalDetailsModel = getArguments().getParcelable(AnimalDetailsActivity.EXTRA_ANIMAL_DETAILS);
-        Toast.makeText(getActivity(), "Other", Toast.LENGTH_SHORT).show();
+        setupChallengesRecyclerView(view);
         return view;
+    }
+
+    private void setupChallengesRecyclerView(View view) {
+        RecyclerView challengesRecyclerView = view.findViewById(R.id.challenges_recyclerView);
+        ChallangesAdapter challangesAdapter = new ChallangesAdapter(getActivity(), animalDetailsModel.getChallenges());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        challengesRecyclerView.setLayoutManager(layoutManager);
+        challengesRecyclerView.setNestedScrollingEnabled(false);
+        challengesRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        challengesRecyclerView.setAdapter(challangesAdapter);
     }
 }
