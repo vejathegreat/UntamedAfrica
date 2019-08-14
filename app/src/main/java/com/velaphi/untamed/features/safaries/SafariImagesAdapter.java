7bd202e991710e7c5961cabf2b69fec22af59c00
veli <velly_mathebula@rocketmail.com>
2019-08-14 07:16:47 +0200

    added safaris feature

---
+++ app/src/main/java/com/velaphi/untamed/features/safaries/SafariImagesAdapter.java
@@ -0,0 +1,73 @@
+package com.velaphi.untamed.features.safaries;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.velaphi.untamed.R;
+import com.velaphi.untamed.injection.GlideApp;
+import com.velaphi.untamed.utils.AppUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SafariImagesAdapter extends RecyclerView.Adapter<SafariImagesAdapter.ViewHolder> {
+    private List<String> imageList = new ArrayList<>();
+
+    public SafariImagesAdapter() {
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_safari_image, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        String imageUrl = imageList.get(position);
+        AppUtil appUtil = new AppUtil();
+        RequestOptions options = new RequestOptions()
+                .error(R.color.colorAccent)
+                .placeholder(R.color.colorAccent)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .priority(Priority.HIGH);
+
+        GlideApp.with(holder.itemView.getContext())
+                .load(appUtil.getImageFromStorage(imageUrl))
+                .apply(options)
+                .centerCrop()
+                .into(holder.safariImageView);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageList.size();
+    }
+
+    void setItems(List<String> imageList) {
+        this.imageList = imageList;
+        notifyDataSetChanged();
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView safariImageView;
+
+        ViewHolder(View view) {
+            super(view);
+            safariImageView = view.findViewById(R.id.safari_imageview);
+        }
+    }
+}
